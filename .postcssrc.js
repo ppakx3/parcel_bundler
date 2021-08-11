740a90e120771e06a7809bb92e62ec88c44f6455
ppakx3 <noth7305@gmail.com>
2021-08-11 21:53:05 +0900

    creat project

---
+++ .postcssrc.js
@@ -0,0 +1,23 @@
+// ESM
+// Common JS
+
+// import autoprefixer from 'autoprefixer'
+
+// const autoprefixer = require('autoprefixer')
+
+// export {
+//   plugins : [
+//     autoprefixer
+//   ]
+// }
+
+// module.exports = {
+//   plugins: [
+//      require('autoprefixer')
+//   ]
+// }
+
+const autoprefixer = require('autoprefixer');
+module.exports = {
+  plugins: [autoprefixer]
+}
